6a24a4b64edabb86f4ec9e9990ec7cf4d4715a6d
Darryl <118641404+cdsiats@users.noreply.github.com>
2025-02-11 22:17:17 +0800

    refactor: removed whitespace token, added test for identifiers and keywords

--- src/lexer.rs
+++ src/lexer.rs
@@ -31,7 +31,6 @@ pub enum TokenType {
 
     Identifier,
 
-    Whitespace,
     LineBreak,
     Comment,
     Error,
@@ -59,24 +58,14 @@ pub fn tokenize(source_code: &str) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut index: usize = 0;
 
-    let whitespace_regex = Regex::new(r"^[ \t]+").unwrap();
     let line_break_regex = Regex::new(r"^(\n|\r|\r\n)").unwrap();
+    //Matches keywords as whole words
+    let keywords_regex = Regex::new(r"^(plugin|use|prop|enum|type|model|String|Number|Boolean|Text|Date)\b").unwrap();
 
     while index < source_code.len() {
         let curr_substring = &source_code[index..];
 
-        if let Some(m) = whitespace_regex.find(curr_substring) {
-            let matched_str = m.as_str();
-            tokens.push(
-                create_token(
-                    TokenType::Whitespace,
-                    matched_str,
-                    index,
-                    index + matched_str.len()
-                )
-            );
-            index += matched_str.len();
-        } else if let Some(m) = line_break_regex.find(curr_substring) {
+        if let Some(m) = line_break_regex.find(curr_substring) {
             let matched_str = m.as_str();
             tokens.push(
                 create_token(
@@ -87,12 +76,32 @@ pub fn tokenize(source_code: &str) -> Vec<Token> {
                 )
             );
             index += matched_str.len();
-        } 
+        } else if let Some(m) = keywords_regex.find(curr_substring) {
+            let matched_keyword = m.as_str();
+            let token_type = match matched_keyword {
+                "plugin" => TokenType::PluginKeyword,
+                "use" => TokenType::UseKeyword,
+                "prop" => TokenType::PropKeyword,
+                "enum" => TokenType::EnumKeyword,
+                "type" => TokenType::TypeKeyword,
+                "model" => TokenType::ModelKeyword,
+                "String" => TokenType::StringType,
+                "Number" => TokenType::NumberType,
+                "Boolean" => TokenType::BooleanType,
+                "Text" => TokenType::TextType,
+                "Date" => TokenType::DateType,
+                _ => TokenType::Identifier,
+            };
+            tokens.push(
+                create_token(token_type, matched_keyword, index, index + matched_keyword.len())
+            );
+            index += matched_keyword.len();
+        }
         else {
             index += 1;
         }
     }
-
+    //Return tokens vector
     tokens
 }
 
@@ -100,18 +109,6 @@ pub fn tokenize(source_code: &str) -> Vec<Token> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn tokenize_whitespace() {
-        let source = " \t ";
-        let tokens = tokenize(source);
-
-        assert_eq!(tokens.len(), 1);
-        if tokens.len() > 0 {
-            assert_eq!(tokens[0].token_type, TokenType::Whitespace);
-            assert_eq!(tokens[0].value, source);
-        }
-    }
 
     #[test]
     fn tokenize_line_break() {
@@ -131,6 +128,80 @@ mod tests {
 
             assert_eq!(tokens[3].token_type, TokenType::LineBreak);
             assert_eq!(tokens[3].value, "\r");
+        }
+    }
+
+    #[test]
+    fn tokenize_keywords() {
+        let source = "plugin use prop enum type model String Number Boolean Text Date";
+        let tokens = tokenize(source);
+
+        println!("{:#?}", tokens);
+
+        assert_eq!(tokens.len(), 11);
+        if tokens.len() >= 11 {
+            assert_eq!(tokens[0].token_type, TokenType::PluginKeyword);
+            assert_eq!(tokens[0].value, "plugin");
+
+            assert_eq!(tokens[1].token_type, TokenType::UseKeyword);
+            assert_eq!(tokens[1].value, "use");
+
+            assert_eq!(tokens[2].token_type, TokenType::PropKeyword);
+            assert_eq!(tokens[2].value, "prop");
+
+            assert_eq!(tokens[3].token_type, TokenType::EnumKeyword);
+            assert_eq!(tokens[3].value, "enum");
+
+            assert_eq!(tokens[4].token_type, TokenType::TypeKeyword);
+            assert_eq!(tokens[4].value, "type");
+
+            assert_eq!(tokens[5].token_type, TokenType::ModelKeyword);
+            assert_eq!(tokens[5].value, "model");
+
+            assert_eq!(tokens[6].token_type, TokenType::StringType);
+            assert_eq!(tokens[6].value, "String");
+
+            assert_eq!(tokens[7].token_type, TokenType::NumberType);
+            assert_eq!(tokens[7].value, "Number");
+
+            assert_eq!(tokens[8].token_type, TokenType::BooleanType);
+            assert_eq!(tokens[8].value, "Boolean");
+
+            assert_eq!(tokens[9].token_type, TokenType::TextType);
+            assert_eq!(tokens[9].value, "Text");
+
+            assert_eq!(tokens[10].token_type, TokenType::DateType);
+            assert_eq!(tokens[10].value, "Date");
+        }
+    }
+
+    #[test]
+    fn tokenize_identifiers() {
+        let source = "Shirts sizes street city User Address customType";
+        let tokens = tokenize(source);
+
+        assert_eq!(tokens.len(), 7);
+        if tokens.len() >= 7 {
+            assert_eq!(tokens[0].token_type, TokenType::Identifier);
+            assert_eq!(tokens[0].value, "Shirts");
+
+            assert_eq!(tokens[1].token_type, TokenType::Identifier);
+            assert_eq!(tokens[1].value, "sizes");
+
+            assert_eq!(tokens[2].token_type, TokenType::Identifier);
+            assert_eq!(tokens[2].value, "street");
+
+            assert_eq!(tokens[3].token_type, TokenType::Identifier);
+            assert_eq!(tokens[3].value, "city");
+
+            assert_eq!(tokens[4].token_type, TokenType::Identifier);
+            assert_eq!(tokens[4].value, "User");
+
+            assert_eq!(tokens[5].token_type, TokenType::Identifier);
+            assert_eq!(tokens[5].value, "Address");
+
+            assert_eq!(tokens[6].token_type, TokenType::Identifier);
+            assert_eq!(tokens[6].value, "customType");
         }
     }
 }
