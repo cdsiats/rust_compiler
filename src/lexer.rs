5e047a314330102f256c357bdbfaf8c292c28f19
Darryl <118641404+cdsiats@users.noreply.github.com>
2025-02-11 19:19:34 +0800

    added, token types, tests for whitespace and line breaks

---
+++ src/lexer.rs
@@ -0,0 +1,124 @@
+use regex::Regex;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenType {
+    PluginKeyword,
+    UseKeyword,
+    PropKeyword,
+    EnumKeyword,
+    TypeKeyword,
+    ModelKeyword,
+    StringType,
+    NumberType,
+    BooleanType,
+    TextType,
+    DateType,
+
+    //Literals
+    StringLiteral,
+    NumberLiteral,
+    BooleanLiteral,
+
+    //Symbols
+    OpenBrace,
+    CloseBrace,
+    OpenParen,
+    CloseParen,
+    OpenSquare,
+    CloseSquare,
+    AtSymbol,
+    Optional,
+
+    Identifier,
+
+    Whitespace,
+    LineBreak,
+    Comment,
+    Error,
+    EOF,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub value: String,
+    pub start: usize,
+    pub end: usize,
+}
+
+
+pub fn tokenize(source_code: &str) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut index: usize = 0;
+
+    let whitespace_regex = Regex::new(r"^[ \t]+").unwrap();
+    let line_break_regex = Regex::new(r"^(\n|\r|\r\n|\n\r)").unwrap();
+
+    while index < source_code.len() {
+        let curr_substring = &source_code[index..];
+
+        if let Some(m) = whitespace_regex.find(curr_substring) {
+            let matched_str = m.as_str();
+            tokens.push(Token {
+                token_type: TokenType::Whitespace,
+                value: matched_str.to_string(),
+                start: index,
+                end: index + matched_str.len(),
+            });
+            index += matched_str.len();
+        } else if let Some(m) = line_break_regex.find(curr_substring) {
+            let matched_str = m.as_str();
+            tokens.push(Token {
+                token_type: TokenType::LineBreak,
+                value: matched_str.to_string(),
+                start: index,
+                end: index + matched_str.len(),
+            });
+            index += matched_str.len();
+        } 
+        else {
+            index += 1;
+        }
+    }
+
+    tokens
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tokenize_whitespace() {
+        let source = " \t ";
+        let tokens = tokenize(source);
+
+        assert_eq!(tokens.len(), 1);
+        if tokens.len() > 0 {
+            assert_eq!(tokens[0].token_type, TokenType::Whitespace);
+            assert_eq!(tokens[0].value, source);
+        }
+    }
+
+    #[test]
+    fn tokenize_line_break() {
+        let source = "\n\r\n\r";
+        let tokens = tokenize(source);
+
+        assert_eq!(tokens.len(), 4);
+        if tokens.len() >= 2 {
+            assert_eq!(tokens[0].token_type, TokenType::LineBreak);
+            assert_eq!(tokens[0].value, "\n");
+
+            assert_eq!(tokens[1].token_type, TokenType::LineBreak);
+            assert_eq!(tokens[1].value, "\r");
+
+            assert_eq!(tokens[2].token_type, TokenType::LineBreak);
+            assert_eq!(tokens[2].value, "\n");
+
+            assert_eq!(tokens[3].token_type, TokenType::LineBreak);
+            assert_eq!(tokens[3].value, "\r");
+        }
+    }
+}
